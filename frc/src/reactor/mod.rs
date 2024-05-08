2934193e18378b0ac8abd9edbe98e86e8aff333e
JoeWildfong <wildfong.joe@gmail.com>
2024-05-08 09:08:25 -0400

    document irqs somewhat

--- frc/src/reactor/mod.rs
+++ frc/src/reactor/mod.rs
@@ -15,26 +15,51 @@ pub mod driver_station;
 mod waker_array;
 
 // HOW INTERRUPTS WORK
+// the FPGA has 32 IRQs, which are allocated to various purposes
+// the first 16 IRQs are used for 8 channels of digital input interrupts
+// each channel can be mapped to a DIO, AI or MXI pin (needs confirmation)
+// each channel has one IRQ that triggers on rising edges and one for falling edges
+// there is also an IRQ which functions as an alarm
+// the alarm can be given a time, and when the FPGA time matches, the IRQ is triggered
+// idk if any other IRQs are used, or what they're used for
+
+// IrqContexts
+// the fpga hands out IrqContext objects
+
+// USING DIGITAL INPUT INTERRUPTS
 // call HAL_InitializeInterrupts to get a HAL_InterruptHandle
 // now you need to call HAL_RequestInterrupts to route a digital source (i.e. a pin) as the interrupt source
 // then call HAL_WaitForInterrupt to block the thread until the interrupt fires
-// HAL_SetInterruptUpSourceEdge exists too - maybe it sets which signal edges to listen for?
-// HAL_ReleaseWaitingInterrupt exists too - maybe it cancels an existing HAL_WaitForInterrupt?
+// HAL_SetInterruptUpSourceEdge exists too - seems to set which signal edges to listen for
+// HAL_ReleaseWaitingInterrupt exists too - seems to force an interrupt to fire
+// when wpihal registers an interrupt, it allocates it an index from 0-7
+// the IRQ corresponding to the index is used for the signal's rising edge
+// the IRQ corresponding to the index + 8 is used for the signal's falling edge
+// unsure, but maybe HAL_SetInterruptUpSourceEdge works by disabling one or both of these IRQs
 
 #[repr(usize)]
 pub enum Irq {
-    Irq0 = 0,
-    Irq1 = 1,
-    Irq2 = 2,
-    Irq3 = 3,
-    Irq4 = 4,
-    Irq5 = 5,
-    Irq6 = 6,
-    Irq7 = 7,
+    Digital0Rising = 0,
+    Digital1Rising = 1,
+    Digital2Rising = 2,
+    Digital3Rising = 3,
+    Digital4Rising = 4,
+    Digital5Rising = 5,
+    Digital6Rising = 6,
+    Digital7Rising = 7,
+    Digital0Falling = 8,
+    Digital1Falling = 9,
+    Digital2Falling = 10,
+    Digital3Falling = 11,
+    Digital4Falling = 12,
+    Digital5Falling = 13,
+    Digital6Falling = 14,
+    Digital7Falling = 15,
+    Alarm = 28,
 }
 
 pub struct IrqReactor {
-    wakers: Arc<Mutex<WakerArray<8>>>,
+    wakers: Arc<Mutex<WakerArray<16>>>,
     shutdown: Arc<AtomicBool>,
 }
 
@@ -43,7 +68,7 @@ impl IrqReactor {
     pub fn new() -> Self {
         let shutdown = Arc::new(AtomicBool::new(false));
         let shutdown_inner = Arc::clone(&shutdown);
-        let wakers: Arc<Mutex<WakerArray<8>>> = Arc::default();
+        let wakers: Arc<Mutex<WakerArray<16>>> = Arc::default();
         let wakers_inner = Arc::clone(&wakers);
 
         std::thread::spawn(move || {
